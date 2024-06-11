0cc99de3b5a8c8277b7c53d1ba3b5dadff232ad8
Jim <jamesgabrielpeterson@gmail.com>
2024-06-10 22:29:05 -0400

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Repair</h1>
+      {/* Create input field with form validation */}
+
+      {/* Create Button */}
+      
+    </div>
+  );
+}
