e0591d7a830b633b2ba861d08696058a17e56f64
Jim <omesadev@outlook.com>
2024-06-25 22:57:28 -0400

    Add better header with Card to landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,11 @@
 'use client';
 
-
+import {Card, CardHeader, CardBody, CardFooter, Divider, Link, Image} from "@nextui-org/react";
 
 import { useFormState } from "react-dom"
 
 import { Button, Input } from "@nextui-org/react"
 import { processZipcode } from "./actions/processZipcode"
-
-
-
-
 
 export default function Home() {
   const [formState, action] = useFormState(processZipcode, {
@@ -18,7 +14,29 @@ export default function Home() {
 
   return (
     <div>
-      <h1 className="text-5xl my-10">Appliance Repair</h1>
+      <Card className="my-10">
+        <CardHeader className="text-center">
+          <h1 className="text-5xl my-10 m-auto">Vacuum Repair</h1>
+        </CardHeader>
+        <CardBody className="flex flex-row justify-evenly">
+          <Card className="w-64 h-48 p-3">
+            <h1 className="text-3xl text-center py-3">EASY</h1>
+            <p className="text-xl text-center">Don't ever carry your applicance across town for a repair ever again.</p>
+          </Card>
+          <Card className="w-64 h-48 p-3">
+            <h1 className="text-3xl text-center py-3">QUICK</h1>
+            <p className="text-xl text-center">We come to you with the right part and fix your appliance on the spot.</p>
+          </Card>
+          <Card className="w-64 h-48 p-3">
+            <h1 className="text-3xl text-center py-3">COMPLETE</h1>
+            <p className="text-xl text-center">We make sure your appliance is fixed and working like brand new before we leave.</p>
+          </Card>
+        </CardBody>
+        <CardFooter>
+
+        </CardFooter>
+      </Card>
+      
 
       <form action={action} >
       <Input 
