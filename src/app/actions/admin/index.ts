6e5b569236277dd4e1a37f5c599e2daa5678f1e3
Jim <jamesgabrielpeterson@gmail.com>
2024-07-10 12:15:19 -0400

    admin dashboard

--- src/app/actions/admin/index.ts
+++ src/app/actions/admin/index.ts
@@ -1,13 +1,34 @@
 'use server';
 
 import { db } from "@/db/db";
+import shapefile from 'shapefile';
+import { FeatureCollection, Feature } from 'geojson';
 
 export default async function getZipcodeDataFromDb() {
-    const zipcodeData = await db.zipcode.findAll();
+    // const zipcodeData = await db.zipcode.findAll();
 
-    if (!zipcodeData) {
-        throw new Error("Could not fetch zipcode data");
+    // if (!zipcodeData) {
+    //     throw new Error("Could not fetch zipcode data");
+    // }
+
+    // return zipcodeData;
+}
+
+// function to load shapefiles
+export async function loadShapeFile(url: string) {
+    const response = await fetch(url);
+    const arrayBuffer = await response.arrayBuffer();
+    const source = await shapefile.open(arrayBuffer)
+    const geoJson: FeatureCollection = { type: 'FeatureCollection', features: [] }
+
+    let result = await source.read();
+    while (!result.done) {
+        if (result.value) {
+            geoJson.features.push(result.value as Feature);
+        }
+        result = await source.read();
     }
 
-    return zipcodeData;
+    return geoJson;
 }
+
--- src/app/admin/Map.tsx
+++ src/app/admin/Map.tsx
@@ -3,19 +3,26 @@
 import { useEffect, useState } from 'react';
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
-import shapefile from 'shapefile';
+import { loadShapeFile } from '../actions/admin';
 import { FeatureCollection, Feature } from 'geojson';
 
 type SomeDatabaseType = {
     [zipcode: string]: number;
   };
 
+  const someDatabase: SomeDatabaseType = {
+    // mock data for zipcode
+    "12345": 20,
+    "67890": 50,
+    "11223": 80
+};
+
 export default function Map(): React.ReactNode {
     const [map, setMap] = useState<L.Map | null>(null);
     const [displayedZipcodes, setDisplayedZipcodes] = useState<string[] | []>([]);
 
     useEffect(() => {
-        const initializeMap = () => {
+        const initializeMap = async () => {
             const mapInstance = L.map('map', {
                 center: [41.77, -72.68],
                 zoom: 12,
@@ -26,59 +33,47 @@ export default function Map(): React.ReactNode {
 
             setMap(mapInstance);
 
-            // function to load shapefiles
-            async function loadShapeFile(url: string) {
-                const response = await fetch(url);
-                const arrayBuffer = await response.arrayBuffer();
-                const source = await shapefile.open(arrayBuffer)
-                const geoJson: FeatureCollection = { type: 'FeatureCollection', features: [] }
-            
-                let result = await source.read();
-                while (!result.done) {
-                    if (result.value) {
-                        geoJson.features.push(result.value as Feature);
+            try {
+                const geoJson: FeatureCollection = await loadShapeFile('../zipcodedata/cb_2018_us_zcta510_500k.shp');
+                const geoJsonLayer = L.geoJSON(geoJson, {
+                    style: feature => {
+                        const zipcode = feature?.properties.ZIPCODE;
+                        const value = zipcode ? getValueForZipcode(zipcode) : 0;
+                        return { color: getColorForValue(value) };
                     }
-                    result = await source.read();
-                }
-
-                return geoJson;
-            }
-
-            loadShapeFile('/pathToShapeFile')
-                .then((geoJson: FeatureCollection) => {
-                    const geoJsonLayer = L.geoJSON(geoJson, {
-                        style: feature => {
-                            const zipcode = feature?.properties.ZIPCODE;
-                            const value = zipcode ? getValueForZipcode(zipcode) : 0;
-                            return { color: getColorForValue(value) }
-                        }
-                    }).addTo(mapInstance);
-
-                    // Update displayed zipcodes on map move or zoom
-                    const updateDisplayedZipcodes = () => {
-                        const bounds = mapInstance.getBounds();
-                        const zipcodes: string[] = [];
-
-                        geoJsonLayer.eachLayer(layer => {
-                            if (layer instanceof L.Polygon && bounds.intersects(layer.getBounds())) {
-                                const properties = (layer.feature as Feature).properties;
-                                if (properties && properties.ZIPCODE) {
-                                    zipcodes.push(properties.ZIPCODE);
-                                }
+                }).addTo(mapInstance);
+    
+                const updateDisplayedZipcodes = () => {
+                    const bounds = mapInstance.getBounds();
+                    const zipcodes: string[] = [];
+    
+                    geoJsonLayer.eachLayer(layer => {
+                        if (layer instanceof L.Polygon && bounds.intersects(layer.getBounds())) {
+                            const properties = (layer.feature as Feature).properties;
+                            if (properties && properties.ZIPCODE) {
+                                zipcodes.push(properties.ZIPCODE);
                             }
-                        });
-
-                        setDisplayedZipcodes(zipcodes);
-                    };
-
-                    mapInstance.on('moveend', updateDisplayedZipcodes);
-                    mapInstance.on('zoomend', updateDisplayedZipcodes);
-                })
-                .catch(error => console.error('Error loading shapefile: ', error));
+                        }
+                    });
+    
+                    setDisplayedZipcodes(zipcodes);
+                };
+    
+                mapInstance.on('moveend', updateDisplayedZipcodes);
+                mapInstance.on('zoomend', updateDisplayedZipcodes);
+            } catch (error) {
+                console.error('Error loading shapefile: ', error);
+            }
         };
 
-        initializeMap();
+        if (!map) {
+            initializeMap();
+        }
     }, [map]);
+
+    const loadAndProcessShapeFile = async (mapInstance: L.Map) => {
+        
+    };
 
     function getValueForZipcode(zipcode: string): number {
         // replace with logic to fetch value from db
@@ -90,13 +85,6 @@ export default function Map(): React.ReactNode {
         const shade = Math.min(value * 2.55, maxColorValue);
         return `rgba(0, ${shade}, 0, 1)`
     }
-
-    const someDatabase: SomeDatabaseType = {
-        // mock data for zipcode
-        "12345": 20,
-        "67890": 50,
-        "11223": 80
-    };
 
     return <div id="map" style={{ width: '100%', height: '600px' }}></div>
 }
